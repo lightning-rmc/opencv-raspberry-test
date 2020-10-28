9a996794f4d246dfba421a44bc459172d22e1381
maSchoeller <git@marvinschoeller.de>
2020-10-28 19:11:10 +0100

    [dev]: add video support

--- Program.cs
+++ Program.cs
@@ -7,11 +7,31 @@ namespace opencv_raspberry_test
     {
         static void Main(string[] args)
         {
-             using var src = Cv2.ImRead("Kamera_2.png");
+            //  using var src = Cv2.ImRead("Kamera_2.png");
 
-            using (new Window("SubMat", src))
+            // using (new Window("SubMat", src))
+            // {
+            //     Cv2.WaitKey();
+            // }
+
+            using var capture = new VideoCapture("sample.mp4");
+            int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+            using (var window = new Window("capture"))
             {
-                Cv2.WaitKey();
+                // Frame image buffer
+                var image = new Mat();
+
+                // When the movie playback reaches end, Mat.data becomes NULL.
+                while (true)
+                {
+                    capture.Read(image); // same as cvQueryFrame
+                    if(image.Empty())
+                        break;
+
+                    window.ShowImage(image);
+                    Cv2.WaitKey(sleepTime);
+                } 
             }
         }
     }
