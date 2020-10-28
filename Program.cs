59cc6fbc240b59e682e0da09c8253e7da43a246b
maSchoeller <git@marvinschoeller.de>
2020-10-28 18:49:50 +0100

    [dev]: inital push

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace opencv_raspberry_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             using var src = Cv2.ImRead("Kamera_2.png");
+
+            using (new Window("SubMat", src))
+            {
+                Cv2.WaitKey();
+            }
+        }
+    }
+}
