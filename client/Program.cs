aabeead6ef290de51bfb43a424025336c53971d3
maSchoeller <git@marvinschoeller.de>
2020-11-04 15:34:04 +0100

    [bug]: fix adress and add remote control

--- client/Program.cs
+++ client/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Threading.Tasks;
-using Grpc.Core;
+﻿using Grpc.Core;
 using Grpc.Net.Client;
 using OpenCvSharp;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace opencv_raspberry_test.client
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static  void Main(string[] args)
         {
             //  using var src = Cv2.ImRead("Kamera_2.png");
 
@@ -16,32 +16,35 @@ namespace opencv_raspberry_test.client
             // {
             //     Cv2.WaitKey();
             // }
-            var channel = GrpcChannel.ForAddress("https://10.0.0.11//50001");
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+
+            var channel = GrpcChannel.ForAddress("https://10.0.0.11:5001", 
+                                            new() { HttpHandler = httpHandler});
 
             var client = new shared.Greeter.GreeterClient(channel);
 
-            var stream =  client.SayHello(new());
-            await foreach (var item in stream.ResponseStream.ReadAllAsync())
+            var stream = client.SayHello(new());
+            using var capture = new VideoCapture("alone_hd.mp4");
+            using (var window = new Window("capture"))
             {
-                using var capture = new VideoCapture("Alone_hd.mp4");
-                int sleepTime = (int)Math.Round(1000 / capture.Fps);
-
-                using (var window = new Window("capture"))
+                while (stream.ResponseStream.MoveNext().Result)
                 {
+
                     // Frame image buffer
                     var image = new Mat();
 
-                    // When the movie playback reaches end, Mat.data becomes NULL.
-                    while (true)
-                    {
-                        capture.Read(image); // same as cvQueryFrame
-                        if (image.Empty())
-                            break;
 
-                        window.ShowImage(image);
-                        Cv2.WaitKey(sleepTime);
-                    }
-                    // Cv2.WaitKey();
+                    capture.Read(image); // same as cvQueryFrame
+                    if (image.Empty())
+                        break;
+
+                    window.ShowImage(image);
+                    Cv2.WaitKey(1);
+
                 }
             }
             // Console.ReadKey();
--- server/Services/GrpcTestServer.cs
+++ server/Services/GrpcTestServer.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using opencv_raspberry_test.shared;
 using System.Threading.Tasks;
 
@@ -6,12 +7,19 @@ namespace opencv_raspberry_test.server.Services
 {
     public class GrpcTestServer : shared.Greeter.GreeterBase
     {
+        private readonly ILogger<GrpcTestServer> logger;
+
+        public GrpcTestServer(ILogger<GrpcTestServer> logger)
+        {
+            this.logger = logger;
+        }
         public override async Task SayHello(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
         {
             while (true)
             {
+                logger.LogInformation("next");
                 await responseStream.WriteAsync(new());
-                await Task.Delay(30_000);
+                await Task.Delay(1_000/120);
             }
         }
     }
